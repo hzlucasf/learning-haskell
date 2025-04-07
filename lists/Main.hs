53e79ea3c63b8425074e76b663e5f3acf6b6a20c
hzlucasf <hzlucasf@gmail.com>
2025-04-07 13:03:33 -0300

    feat: update lists/Main.hs

--- lists/Main.hs
+++ lists/Main.hs
@@ -2,9 +2,15 @@ module Main where
 
 import Text.Printf
 
+{-
 foo :: [Int] -> Int
 foo [] = 0
 foo (h:t) = h + (foo t)
+-}
+
+double :: [Int] -> [Int]
+double [] = []
+double (h:t) = 2 * h:double t
 
 main :: IO ()
-main = printf "(foo [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]): %d\n" (foo [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+main = printf "(double [1, 2, 3, 4, 5]): %s\n" (show (double [1, 2, 3, 4, 5]))

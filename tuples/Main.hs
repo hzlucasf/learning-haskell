6cf06378b3f8be1b87f0c08c104d3798d2568260
hzlucasf <hzlucasf@gmail.com>
2025-04-15 20:29:11 -0300

    feat: add tuples/Main.hs

---
+++ tuples/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+p :: (Double, Double)
+p = (0.0, 0.0)
+
+t :: (Double, Double, Double)
+t = (3.0, 4.0, 6.0)
+
+u :: (String, Bool)
+u = ("Ada Lovelace", True)
+
+main :: IO ()
+main = putStrLn "Tuples!"
